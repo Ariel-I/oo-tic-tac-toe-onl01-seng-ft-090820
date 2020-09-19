3e36ffcbf2ec10e5ad696b9a9be73a9d7ce5c1c6
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-19 20:46:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,6 +55,12 @@ class TicTacToe
   def won?
     WIN_COMBINATIONS.detect do |win_combo|
       if (@board[win_combo][0] == "X") && (@board[win_combo][1] == "X") && (@board[win_combo][2]) == "X")
+        return "X"
+      else 
+        (@board[win_combo][0] == "O") && (@board[win_combo][1] == "0") &&
+        (@board[win_combo][2] == "O")
+        return "O"
+      end 
   end
   
   def full?
